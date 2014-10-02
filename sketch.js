eede3f4fd4a6e71d34eb2b05bab6edde4414545e
csjoo65 <csjoo65@naver.com>
2014-10-02 16:24:16 +0900

    5

--- sketch.js
+++ sketch.js
@@ -1,10 +1,10 @@
 function setup() {
   createCanvas(640, 480);
-  background(121,181,255);
+  background(121,181, 255);
   noFill();
 }
 function draw() {
-  background(121,181,255);
+  background(121,181, 255);
   ellipse(50,50,50,50);
 if (mouseX < 300) {
   ellipse(200, 200, 100, 100);
